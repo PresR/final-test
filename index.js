c444b93b53ece79e46541a7d6a183eaf29f0eb88
PresR <152711990+PresR@users.noreply.github.com>
2024-02-21 03:28:44 +0000

    An extra line

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 // write some code to print "Hello World" to the console below
 console.log("Hello World");
 console.log("hi");
+// This is a single-line comment
+// write some code to print "Hello World" to the console below
+console.log("Hello World");
+console.log("hi");
